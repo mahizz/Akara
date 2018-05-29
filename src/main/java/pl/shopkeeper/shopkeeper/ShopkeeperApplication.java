29e9e20ba716d780a008ddbef3bbc9ccd8884501
PJ <przemsylaw.jozwik@aegton.com>
2018-05-29 17:43:33 +0200

    init commit

---
+++ src/main/java/pl/shopkeeper/shopkeeper/ShopkeeperApplication.java
@@ -0,0 +1,12 @@
+package pl.shopkeeper.shopkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopkeeperApplication.class, args);
+	}
+}
